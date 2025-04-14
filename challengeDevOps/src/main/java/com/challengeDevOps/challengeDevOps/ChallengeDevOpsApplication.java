f83fbcfb4bc5a9e1d1b212754c9e26b3f84fbd91
julesV <julesvallet4@gmail.com>
2025-04-14 14:09:41 +0200

    passage au projet spring boot

---
+++ challengeDevOps/src/main/java/com/challengeDevOps/challengeDevOps/ChallengeDevOpsApplication.java
@@ -0,0 +1,13 @@
+package com.challengeDevOps.challengeDevOps;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChallengeDevOpsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChallengeDevOpsApplication.class, args);
+	}
+
+}
